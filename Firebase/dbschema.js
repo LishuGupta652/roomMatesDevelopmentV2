39804d35188dd9c5157bb19a715f9c88c099d33e
lishu gupta <lishugupta652@gmail.com>
2019-07-08 15:34:05 +0530

    initial commit

---
+++ Firebase/dbschema.js
@@ -0,0 +1,15 @@
+let db = [
+    rooms: [
+        {
+            "title": "yet another rooms for usage",
+            "roomId": "last rooms ",
+            "desc": " discount price room for api / rooms  ",
+            "price": 125,
+            "rating": 2.2,
+            "facilities": [
+                "first"
+            ],
+            "available": false
+        }
+    ]
+]
